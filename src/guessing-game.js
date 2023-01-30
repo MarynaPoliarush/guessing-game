87799d8d04f78d9ce4e75c67d8017bf599e10edb
MarynaPoliarush <106143753+MarynaPoliarush@users.noreply.github.com>
2023-01-30 15:45:20 +0200

    Update guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,39 @@
+let number;
 class GuessingGame {
-    constructor() {}
-
+    constructor() {
+        this.minRange = null;
+        this.maxRange = null;
+        this.guessResult = null;
+    }
+    
     setRange(min, max) {
-
+        this.minRange = min;
+        this.maxRange = max;
+        
     }
 
     guess() {
-
+        this.guessResult= Math.round( (this.maxRange-this.minRange)/2 + this.minRange) ;
+        console.log('logged guess',this.guessResult)
+        return this.guessResult;
     }
 
     lower() {
-
+        console.log('logged lower 1',this.guessResult)
+        
+            console.log('logged lower',this.guessResult)
+            this.maxRange = Math.round( (this.maxRange-this.minRange)/2 + this.minRange);
+        
     }
 
     greater() {
-
+       
+            console.log('logged greater',this.guessResult)
+            this.minRange = Math.round( (this.maxRange-this.minRange)/2 + this.minRange);
+           
+        
     }
+
 }
 
 module.exports = GuessingGame;
